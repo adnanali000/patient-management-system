33be523386194de540abe7b2ae23b8d8927ef18f
adnanali000 <adnan.museel123@gmail.com>
2026-05-06 01:35:47 +0500

    get all patients endpoint

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,32 @@
+package com.pm.patientservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pm.patientservice.service.PatientService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private PatientService patientService;
+
+    public PatientController(PatientService patientService){
+        this.patientService = patientService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+    
+}
